207918d35fc54bf357665e79eae4ab1c235221f5
khaizerdn <khaizerdn@gmail.com>
2025-05-23 19:45:52 +0800

    Expo update

--- web/src/config.jsx
+++ web/src/config.jsx
@@ -3,7 +3,7 @@ import React, { useMemo } from 'react';
 
 const configSettings = {
   idleTimeout: 60000, // 60 seconds in milliseconds
-  showTimers: true, // Must be true to show the timer
+  showTimers: false, // Must be true to show the timer
 };
 
 const activeTimers = {
